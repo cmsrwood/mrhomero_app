2ce6ed59b2faa367a66c172d609608e23dd56f36
cmsrwood <114179857+cmsrwood@users.noreply.github.com>
2025-03-27 06:36:59 -0500

    Navegacion en carpeta navigation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './screens/navbar/Navbar';
+import Navbar from './navigation/Navbar';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
