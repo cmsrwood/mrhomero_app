1f27194dfcd5cf9c0e347119cebda0b3d658a844
Abjura <107774277+Abjuraa@users.noreply.github.com>
2025-04-01 10:50:26 -0500

    crear parte visual de registrar
    se creo el archivo para registrar usuarios

---
+++ screens/default/RegistrarScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import axios from 'axios'
+
+export default function RegistrarScreen() {
+    return (
+        <View>
+            <Text>Registrar</Text>
+        </View>
+    )
+}
