ccd63454af3c780fa06827561f23e1fa65c5dd6c
Abjura <107774277+Abjuraa@users.noreply.github.com>
2025-04-03 16:55:35 -0500

    Footer
    se creo el footer y se agrego en el defaultLayout

--- components/DefaultLayout.tsx
+++ components/DefaultLayout.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { View, Text, ScrollView, StyleSheet, Image } from "react-native";
-import mrhomeroImg from "../assets/favicon.png";
+import { View, ScrollView, StyleSheet } from "react-native";
 import globalStyles from "../styles/globalStyles";
 import { useNavigation } from "@react-navigation/native";
+import Footer from "./Footer";
 
 export default function DefaultLayout({ children }) {
     const navigation = useNavigation();
@@ -11,6 +11,7 @@ export default function DefaultLayout({ children }) {
         <View style={globalStyles.containerfluid}>
             <ScrollView contentContainerStyle={styles.content}>
                 {children}
+                <Footer />
             </ScrollView>
         </View>
     );
@@ -20,51 +21,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#F5F5F5",
-        justifyContent: "space-between",
-    },
-    header: {
-        backgroundColor: "#2B3035",
-        padding: 15,
-        alignItems: "center",
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-    },
-    headerText: {
-        color: "#FFF",
-        fontSize: 18,
-        fontWeight: "bold",
     },
     content: {
         flexGrow: 1,
         paddingVertical: 20,
         paddingBottom: 70,
     },
-    footer: {
-        backgroundColor: "#2B3035",
-        paddingVertical: 10,
-        justifyContent: "center",
-        alignItems: "center",
-        height: 60,
-    },
-    navbar: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        width: "100%",
-        paddingHorizontal: 20,
-    },
-    ButtonNavbar: {
-        padding: 10,
-        borderRadius: 8,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    ButtonNavbarText: {
-        color: "#FFF",
-        fontSize: 14,
-    }
 });
---
+++ components/Footer.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import mrhomeroImg from "../assets/favicon.png";
+import { Linking, Alert } from "react-native";
+
+
+export default function Footer() {
+    const openLink = async (url) => {
+        const link = await Linking.canOpenURL(url);
+        if (link) {
+            await Linking.openURL(url);
+        } else {
+            Alert.alert("No se pudo abrir el enlace", url);
+            console.log("No se pudo abrir el enlace", url);
+        }
+    }
+
+    return (
+        <View style={styles.footer}>
+            <View style={styles.line} />
+            <Image source={mrhomeroImg} style={styles.imgFooter} />
+            <View style={styles.containerFooter}>
+                <View style={styles.infoContent}>
+                    <Text style={styles.textBold}>
+                        Mr. Homero: La receta del sabor que te hará volver por más.
+                    </Text>
+                    <Text style={styles.contentText}>
+                        Disfruta de nuestras irresistibles hamburguesas, perros calientes, papas crocantes, bebidas refrescantes y mucho más. ¡El sabor único de Mr. Homero te espera! Conoce nuestro menú y vive una experiencia deliciosa.
+                    </Text>
+                </View>
+                <View style={{ flexDirection: "column", justifyContent: "space-between" }}>
+                    <View style={styles.infoContent}>
+                        <Text style={styles.textBold}>
+                            Contacto
+                        </Text>
+                        <Text style={styles.contentText}>
+                            Cl. 25 Sur #6-30, San Cristóbal
+                            Bogotá, Cundinamarca
+                            Llamanos: 300 123 4567
+                            Correos: 4bDd0@example.com
+                            Trabaja con nosotros
+                        </Text>
+                    </View>
+                    <View style={styles.infoContent}>
+                        <Text style={styles.textBold}>
+                            Horarios de atención
+                        </Text>
+                        <Text style={styles.contentText}>
+                            Martes a Viernes de 10:00 a 20:00 {"\n"}
+                            Sabados de 10:00 a 18:00
+                        </Text>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.line} />
+            <View style={styles.infoEnd}>
+                <View style={styles.icons}>
+                    <TouchableOpacity onPress={() => openLink("https://www.facebook.com/p/Comidas-Rapidas-Mr-Homero-100050735053665/?locale=es_LA")}>
+                        <Ionicons name="logo-facebook" size={23} color="#ccc" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => openLink("https://www.instagram.com/mrhomerocomidasrapidas/")}>
+                        <Ionicons name="logo-instagram" size={23} color="#ccc" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => openLink("https://www.tiktok.com/@mrhomerocomidasrapidas")}>
+                        <Ionicons name="logo-tiktok" size={23} color="#ccc" />
+                    </TouchableOpacity>
+                </View>
+                <Text style={styles.contentText}>Mr. Homero © 2025. Todos los derechos reservados.</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    footer: {
+        backgroundColor: "#2B3035",
+        width: "100%",
+        position: "relative",
+    },
+    line: {
+        width: "100%",
+        height: 0.4,
+        backgroundColor: "#ccc",
+        marginVertical: 20,
+    },
+    imgFooter: {
+        width: 60,
+        height: 60,
+        alignSelf: "flex-start",
+        marginLeft: 20,
+    },
+    containerFooter: {
+        display: "flex",
+        flexDirection: "row",
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    textBold: {
+        fontWeight: "bold",
+        marginBottom: 10,
+        color: "#fff",
+        fontSize: 13,
+    },
+    contentText: {
+        color: "#ccc",
+        fontSize: 12,
+    },
+    infoContent: {
+        marginLeft: 20,
+        marginRight: 20,
+        width: 150,
+    },
+    infoEnd: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    icons: {
+        flexDirection: "row",
+        alignSelf: "flex-start",
+        gap: 10,
+        marginLeft: 20,
+        marginBottom: 15
+    }
+});
