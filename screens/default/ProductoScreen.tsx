849706f92641c9e2193664f7b821bffc10fce11e
johansmc <163949679+johansmc@users.noreply.github.com>
2025-04-08 00:15:19 -0500

    Producto
    Arreglos en la estructura del apartado de producto default

--- screens/default/ProductoScreen.tsx
+++ screens/default/ProductoScreen.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { View, Text,Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import DefaultLayout from '../../components/DefaultLayout'
 import globalStyles from '../../styles/globalStyles';
 import useMenu from "../../hooks/useMenu"
+import { NumericFormat } from 'react-number-format';
 import { useRoute } from '@react-navigation/native';
 
 
@@ -14,38 +15,42 @@ export default function ProductoScreen() {
     <DefaultLayout>
       <View>
         <Text style={globalStyles.title}>{pro_nom}</Text>
-        <View style={styles.content}>
-          <Image source={{ uri: producto.pro_foto }} style={styles.image}/>
-          <View style={styles.desp}>
-            <Text style={globalStyles.fontHomero}>{producto.pro_precio}</Text>
-          </View>
+        <View >
+          <Image source={{ uri: producto.pro_foto }} style={styles.image} />
+        </View>
+        <View style={styles.desp}>
+          <Text style={styles.desc}>{producto.pro_desp}</Text>
+          <Text style={styles.puntos}>Puntos: {producto.pro_puntos}</Text>
+          <NumericFormat style={styles.precio} value={producto.pro_precio} displayType={'text'} thousandSeparator='.' decimalSeparator=',' prefix={'$'} />
         </View>
       </View>
     </DefaultLayout>
   )
 }
 const styles = StyleSheet.create({
-  content:{
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginVertical: 0,
-    height:400,
-    maxWidth:'100%',
-  },
-  image:{
-    flex:2,
-    width:'60%',
-    height:'100%',
+ 
+  image: {
+    alignSelf: 'center',
+    width: 340,
+    height: 340,
+    borderColor: '#B7B7B7',
   },
   desp:{
-    display: 'flex',
-    flexDirection: 'column',
-    width: '40%',
-    height: '100%',
-    alignItems: 'center',
-
+    marginLeft: 40,
+    marginTop: 10
+  },
+  desc:{
+    color: '#fff',
+    fontSize: 20
+  },
+  puntos:{
+    color:'#ccc',
+    fontSize:20
+  },
+  precio:{
+    fontSize:40,
+    fontWeight:'bold',
+    color:'#FEBA17'
   }
 
 })
