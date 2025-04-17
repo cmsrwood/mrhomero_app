cbe0c1e549112e068d7823ee768ac0dd51e3343e
johansmc <163949679+johansmc@users.noreply.github.com>
2025-04-16 19:32:00 -0500

    Imagenes Repository
    actualización de menu repository

--- repositories/ImagenesRepository.js
+++ repositories/ImagenesRepository.js
@@ -2,11 +2,7 @@ import API from "../config/api";
 
 export default class ImagenesRepository {
     static async subirImagen(data) {
-        const response = await API.post("/imagenes/subir", data,{
-            headers: {
-                'Content-Type': 'multipart/form-data',
-            },
-        }); 
+        const response = await API.post("/imagenes/subir", data,); 
 
         return response;
     }
