f9670f2a4a23cd16bf1e59904a016507e788171e
cmsrwood <114179857+cmsrwood@users.noreply.github.com>
2025-04-20 18:56:36 -0500

    sin mensaje

--- hooks/useMenu.js
+++ hooks/useMenu.js
@@ -8,7 +8,6 @@ export default function useMenu(type, params = {}) {
     const [refreshTrigger, setRefreshTrigger] = useState(0);
 
     const refetch = () => {
-        console.log('Actualizando...')
         setRefreshTrigger(prev => prev + 1);
     };
 
