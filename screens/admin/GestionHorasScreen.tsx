693423183b178fa10e210124e187f24399ff4812
Abjura <107774277+Abjuraa@users.noreply.github.com>
2025-04-30 19:19:25 -0500

    Empleado Frontend
    se creo la parte visual de los empleados totalmente funcionando con la Api

--- screens/admin/GestionHorasScreen.tsx
+++ screens/admin/GestionHorasScreen.tsx
@@ -1,13 +1,238 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, StyleSheet, Image, Platform } from 'react-native'
 import AdminLayout from '../../components/AdminLayout'
+import useEmpleados from '../../hooks/useEmpleados'
+import globalStyles from '../../styles/globalStyles'
+import { Picker } from '@react-native-picker/picker';
+import { Ionicons } from '@expo/vector-icons'
+import moment from 'moment'
 
-export default function GestionHorasScreen() {
+
+export default function GestionHorasScreen({ route }) {
+
+    const { id } = route.params
+    const { data: empleado, loading: loadingEmpleados, error: errorEmpleados, refetch: refetchEmpleados } = useEmpleados('Empleado', { id_empleado: id });
+
+
+    const anoActual = new Date().getFullYear();
+    const anoInicio = 2024;
+
+    const anos = Array.from({ length: (anoActual - anoInicio + 1) }, (_, i) => anoInicio + i);
+
+    const meses = [
+        { label: 'Enero', value: 1 },
+        { label: 'Febrero', value: 2 },
+        { label: 'Marzo', value: 3 },
+        { label: 'Abril', value: 4 },
+        { label: 'Mayo', value: 5 },
+        { label: 'Junio', value: 6 },
+        { label: 'Julio', value: 7 },
+        { label: 'Agosto', value: 8 },
+        { label: 'Septiembre', value: 9 },
+        { label: 'Octubre', value: 10 },
+        { label: 'Noviembre', value: 11 },
+        { label: 'Diciembre', value: 12 },
+    ];
+
+    const [ano, setAno] = useState(anoActual);
+    const [mes, setMes] = useState(new Date().getMonth() + 1);
+    const [horasEsperdas, setHorasEsperdas] = useState(230);
+    const [horasExtra, setHorasExtra] = useState(0);
+
+    const { data: horasTrabajadas, loading: loadingHorasTrabajadas, error: errorHorasTrabajadas, refetch: refetchHorasTrabajadas } = useEmpleados('HorasTrabajadasPorMes', { id_empleado: id, mes: mes, ano: ano });
+    const { data: diasTrabajados, loading: loadingDiasTrabajados, error: errorDiasTrabajados, refetch: refetchDiasTrabajados } = useEmpleados('DiasTrabajados', { id_empleado: id, mes: mes, ano: ano });
+
+    useEffect(() => {
+        refetchEmpleados();
+        refetchHorasTrabajadas();
+        refetchDiasTrabajados();
+        setHorasExtra(horasTrabajadas?.[0]?.horas - horasEsperdas)
+    }, [mes, ano]);
+
+
+        function diaEspanol(dia) {
+        switch (dia) {
+            case 'Monday':
+                return 'Lunes';
+            case 'Tuesday':
+                return 'Martes';
+            case 'Wednesday':
+                return 'Miercoles';
+            case 'Thursday':
+                return 'Jueves';
+            case 'Friday':
+                return 'Viernes';
+            case 'Saturday':
+                return 'Sabado';
+            case 'Sunday':
+                return 'Domingo';
+            default:
+                return dia;
+        }
+    }
+
+
     return (
         <AdminLayout>
-            <View>
-                <Text>gestion horas</Text>
+            <View style={styles.container}>
+                <Text style={[globalStyles.title, { fontSize: 42, marginTop: 20, maxWidth: 300 }]}> Horas de {empleado.user_nom}</Text>
+                <View>
+                    <Image style={styles.imageEmpleado} source={{ uri: empleado.user_foto }}></Image>
+                    <Text style={styles.nameEmpleado}>{empleado.user_nom} {empleado.user_apels}</Text>
+                    <View style={{ flexDirection: 'row', gap: 10, justifyContent: 'center' }}>
+                        <Picker
+                            style={Platform.OS === 'ios' ? styles.pickerIOS : styles.picker}
+                            selectedValue={ano}
+                            onValueChange={(itemValue) => setAno(itemValue)}
+                        >
+                            {anos.map((y) => (
+                                <Picker.Item key={y} label={y.toString()} value={y} />
+                            ))}
+
+                        </Picker>
+                        <Picker
+                            style={Platform.OS === 'ios' ? styles.pickerIOS : styles.picker}
+                            selectedValue={mes}
+                            onValueChange={(value) => setMes(value)}
+                        >
+                            {meses.map((m) => (
+                                <Picker.Item key={m.value} label={m.label} value={m.value} />
+                            ))}
+
+                        </Picker>
+                    </View>
+
+                    <View style={styles.horasContainer}>
+                        <View style={styles.horaContent}>
+                            <View style={{ flexDirection: 'row', gap: 5 }} >
+                                <Ionicons name="time-outline" size={20} color="#3B82F6"></Ionicons>
+                                <Text style={{ color: '#fff', marginBottom: 5, fontSize: 12, }}>Esperadas</Text>
+                            </View>
+                            <Text style={{ color: '#fff', marginBottom: 5, fontSize: 22, fontWeight: 'bold', alignSelf: 'flex-start' }}>{horasEsperdas} h</Text>
+                        </View>
+                        <View style={styles.horaContent}>
+                            <View style={{ flexDirection: 'row', gap: 5 }} >
+                                <Ionicons name="time-outline" size={20} color="#10B981"></Ionicons>
+                                <Text style={{ color: '#fff', marginBottom: 5, fontSize: 12 }}>registradas</Text>
+                            </View>
+                            <Text style={{ color: '#fff', marginBottom: 5, fontSize: 22, fontWeight: 'bold', alignSelf: 'flex-start' }}>  {horasTrabajadas?.[0]?.horas ? horasTrabajadas?.[0]?.horas : 0} h</Text>
+                        </View>
+                        <View style={styles.horaContent}>
+                            <View style={{ flexDirection: 'row', gap: 5 }} >
+                                <Ionicons name="time-outline" size={20} color="#FBBF24"></Ionicons>
+                                <Text style={{ color: '#fff', marginBottom: 5, fontSize: 12 }}>Extra</Text>
+                            </View>
+                            <Text style={{ color: '#fff', marginBottom: 5, fontSize: 22, fontWeight: 'bold', alignSelf: 'flex-start' }}>{horasExtra > 0 ? horasExtra : 0} h</Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.table}>
+                        <View style={styles.column}>
+                            <View style={styles.headerTable}>
+                                <Text style={{ color: '#fff', marginBottom: 5, fontSize: 15, fontWeight: 'bold' }}>Dia</Text>
+                                <Text style={{ color: '#fff', marginBottom: 5, fontSize: 15, fontWeight: 'bold' }}>Fecha</Text>
+                                <Text style={{ color: '#fff', marginBottom: 5, fontSize: 15, fontWeight: 'bold' }}>Inicio</Text>
+                                <Text style={{ color: '#fff', marginBottom: 5, fontSize: 15, fontWeight: 'bold' }}>Fin</Text>
+                            </View>
+
+                            
+                            {diasTrabajados.length === 0 ? <Text style={{ color: '#fff', marginBottom: 5, fontSize: 15, fontWeight: 'bold', margin: 10, textAlign: 'center' }}>No hay horas registradas en el mes seleccionado</Text> : null}
+                            {diasTrabajados.map((dia) => (
+                                <View style={styles.row}>
+                                    <Text style={{ color: '#fff' }}>{diaEspanol(moment(dia.fecha).format('dddd'))}</Text>
+                                    <Text style={{ color: '#fff' }}>{moment(dia.fecha).format('DD-MM-YYYY')}</Text>
+                                    <Text style={{ color: '#fff' }}>{moment(dia.hora_inicio).format('hh:mm:ss A')}</Text>
+                                    <Text style={{ color: '#fff' }}>{moment(dia.hora_fin).format('hh:mm:ss A')}</Text>
+                                </View>
+                            ))}
+
+                        </View>
+
+                    </View>
+                </View>
             </View>
         </AdminLayout>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        paddingHorizontal: 20
+    },
+    imageEmpleado: {
+        width: 120,
+        height: 120,
+        borderRadius: 200,
+        alignSelf: 'center',
+        borderWidth: 1,
+        borderColor: '#FFC107',
+        marginBottom: 13
+    },
+    nameEmpleado: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#fff',
+        textAlign: 'center'
+    },
+    picker: {
+        backgroundColor: '#2B3035',
+        color: '#fff',
+        width: 150,
+        height: 50,
+        marginVertical: 10,
+    },
+    pickerIOS: {
+        flex: 1,
+        overflow: 'hidden',
+        justifyContent: 'center',
+        height: 50,
+        marginVertical: 10
+    },
+    table: {
+        alignItems: 'center',
+        width: '100%'
+    },
+    row: {
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderColor: '#BDC3C7',
+        marginTop: 10,
+        paddingBottom: 10,
+        width: '100%',
+        justifyContent: 'space-around'
+    },
+    column: {
+        borderWidth: 1,
+        borderColor: '#BDC3C7',
+        borderRadius: 4,
+        marginBottom: 10,
+    },
+    headerTable: {
+        flexDirection: 'row',
+        backgroundColor: '#4A5159',
+        padding: 10,
+        justifyContent: 'space-between',
+        width: '100%',
+        paddingHorizontal: 25
+    },
+    horasContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        marginBottom: 15,
+        width: '100%'
+    },
+    horaContent: {
+        alignItems: 'center',
+        flexDirection: 'column',
+        backgroundColor: '#4A5159',
+        padding: 10,
+        marginHorizontal: 6,
+        borderWidth: 1,
+        borderColor: '#BDC3C7',
+        borderRadius: 4,
+        width: '30%'
+    }
+});
--- screens/admin/HorasEmpleadosScreen.tsx
+++ screens/admin/HorasEmpleadosScreen.tsx
@@ -1,14 +1,79 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, StyleSheet, Image, TouchableOpacity, Platform } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 import AdminLayout from '../../components/AdminLayout'
+import globalStyles from '../../styles/globalStyles'
+import { Ionicons } from '@expo/vector-icons'
+import useEmpleados from '../../hooks/useEmpleados'
 
 
 export default function HorasEmpleadosScreen() {
+
+    const { data: empleados, loading: loadingEmpleados, error: errorEmpleados, refetch: refetchEmpleados } = useEmpleados('Empleados');
+    const navigation = useNavigation();
+
     return (
         <AdminLayout>
-            <View>
-                <Text>horas empleados</Text>
+            <View style={styles.container}>
+                <Text style={[globalStyles.title, { fontSize: 40, marginTop: 30, marginBottom: 30 }]}>Horas de empleados</Text>
+                {empleados.map((empleado) => (
+                    <View style={styles.containerInfo}>
+                        <Image style={styles.image} source={{ uri: empleado.user_foto }}></Image>
+                        <View style={styles.containerData}>
+                            <Text style={{ fontWeight: 'bold', color: '#fff', fontSize: 16, maxWidth: 150, marginBottom: 6 }}>{empleado.user_nom} {empleado.user_apels}</Text>
+                            <View style={{ flexDirection: 'row', gap: 5, }}>
+                                <Ionicons name="call-outline" style={{ color: '#FFC107', fontSize: 14, marginTop: 2, marginRight: 2 }}></Ionicons>
+                                <Text style={{ color: '#FFC107', fontSize: 14 }}>{empleado.user_tel ? empleado.user_tel : 'Sin teléfono'}</Text>
+                            </View>
+                        </View>
+                        <TouchableOpacity style={styles.button}>
+                            <Ionicons name="information-circle-outline" size={20} color="#fff"
+                                onPress={() => navigation.navigate('GestionHoras', { id: empleado.id_user })}
+                            ></Ionicons>
+                        </TouchableOpacity>
+                    </View>
+                ))}
             </View>
-        </AdminLayout>
+        </AdminLayout >
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    image: {
+        width: 70,
+        height: 70,
+        borderRadius: 35
+    },
+    containerInfo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#3A4149',
+        borderWidth: 1,
+        borderColor: '#4A5159',
+        borderRadius: 8,
+        width: '90%',
+        height: 100,
+        padding: 10,
+        gap: 20,
+        marginBottom: 14
+    },
+    containerData: {
+        flex: 1,
+        justifyContent: 'center',
+        alignSelf: 'center',
+
+    },
+    button: {
+        backgroundColor: '#FFC107',
+        borderRadius: 100,
+        height: 36,
+        width: 36,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 20
+    }
+})
